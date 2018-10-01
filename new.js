46d2e1043ea8da6cff872f6e57c15a44933b1e60
Hesham <alkurdi.hesham@gmail.com>
2018-10-01 13:50:07 +0200

    js

--- new.js
+++ new.js
@@ -1,1 +1,20 @@
-  console.log("Hello world");
+// var i = 0
+// for ( i = 0; i < 20; i++) {
+//   console.log("HelloWorld");
+// }
+// var t=0;
+// for (var i = 0; i <= 100; i++) {
+//     if (i<100) {
+//       t += i
+//     }
+//
+// }
+// console.log(t);
+var t=0;
+for (var i = 0; i <= 100;i=i+2) {
+    if (i<100) {
+      t += i
+    }
+
+}
+console.log(t);
