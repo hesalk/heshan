a51e880030ed025c777ef1de07778c0b7048a12a
Hesham <alkurdi.hesham@gmail.com>
2018-10-11 08:12:20 +0200

    lab2

--- javascriptedon.js
+++ javascriptedon.js
@@ -2,6 +2,7 @@ function mf () {
 var div = document.querySelector("#no-text");
 var p = document.createElement("p");
 p.textContent = "HelloWorld";
+console.log();
 div.appendChild(p);
 var main = document.querySelector("main")
 var rmv = document.querySelectorAll(".remove-me");
@@ -9,3 +10,19 @@ for (var i = 0; i < rmv.length; i++) {
   main.removeChild(rmv[i]);
   }
   }
+function ff () {
+  var firstbtn = document.querySelector("button");
+
+}
+
+
+
+ function onClick(e) {
+  var button = e.target;
+  console.log("Button clicked: " + button.textContent);
+ }
+ var myButtons = document.querySelectorAll("button");
+ for (var i = 0; i < myButtons.length; i++) {
+ var button = myButtons[i];
+ button.addEventListener("click", onClick);
+ }
